d18c02a258c6a3f86bb9ad280dfaa0a7eaf90f03
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-04-19 20:59:08 +0530

    day 1

---
+++ Day 1/Use Comments.js
@@ -0,0 +1,11 @@
+// A good Developer always uses comments 
+
+console.log("\n\t\t Hey There!!"); //This is an in-line comment and I can use this so you know what I did on this single line of code
+
+/* This is called multi-line comment.
+I can now comment not just a single line,
+but multiple lines....*/ 
+
+console.log("\n\t Do you use comments to make your code readable ?");
+
+//Be responsible, always use comments !!
