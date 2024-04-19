c14a52c2347733fa318b1fdc4fdb6387ba7d4390
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-04-19 21:09:07 +0530

    day 2

---
+++ Day 2/declaration.js
@@ -0,0 +1,7 @@
+// Declaring variable in javascript
+
+var myName; //Declared myName as a variable using var keyword
+
+myName= "Ritanjit Das";
+
+console.log("\n\t\t Hi there! I'm " + myName); //Printed my name
---
+++ Day 2/variables.js
@@ -0,0 +1,7 @@
+// Assigning values to variable
+
+var a; // declared a as a variable
+
+a=7; //assigned a value to variable
+
+console.log("\n\t\t Siuuuuu!! Goal by CR" + a + "!!"); //printed the variable
