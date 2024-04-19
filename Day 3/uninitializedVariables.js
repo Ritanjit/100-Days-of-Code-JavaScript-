2470a55fcad8ec12ce24e5ed10650e74d9d98824
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-04-19 21:11:11 +0530

    Create uninitializedVariables.js

---
+++ Day 3/uninitializedVariables.js
@@ -0,0 +1,11 @@
+/* Uninitialized Variables */
+
+var a; // declared uninitialized variable
+
+// value of undefined variable
+console.log("\n\t The value of undefined variable 'a' is -->" + a);
+
+// performing mathematics operations on undefined variable
+console.log("\n\t\tDoing math on undefined variable 'a', we get -->" + (a + 100));
+
+// Fun Fact : NaN stands for 'Not a variable'
