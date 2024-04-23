3da25e60a09335558d8f322819c3b1e8e88a110c
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-04-23 23:32:10 +0530

    Create functionVariable.js

---
+++ Day 14/functionVariable.js
@@ -0,0 +1,14 @@
+
+/* Storing variable in a Function */
+
+function calcAverage(a,b,c){
+    return (a+b+c)/3;
+  }
+  
+  let avg = calcAverage(83,65,90);
+  
+  console.log("\n\n Your Average Score : " + avg.toFixed(2));
+  
+  // TIP : toFixed(n) decides the number of decimal places 
+  
+  
