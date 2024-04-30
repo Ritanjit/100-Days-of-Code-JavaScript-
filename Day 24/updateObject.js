8230e20c6ab98444ff46a3977835f93c1d685966
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-05-01 02:41:54 +0530

    Update updateObject.js

--- Day 24/updateObject.js
+++ Day 24/updateObject.js
@@ -17,10 +17,12 @@ let bestMovies = {
 // Updating objects - Dot notation 
 bestMovies.Rocky = ['Action','Sport','Drama'];          // Modifying data
 bestMovies.WALL_E = ['Adventure','Drama'];              // Adding new data
+delete bestMovies.Inception;                            // Deleting data
 
 // Updating objects - Bracket notation
 bestMovies["Fight club"] = ['Thriller','Action'];       // Modifying data
 bestMovies['Grave of Fireflies'] = ['Drama','War'];     // Adding new data
+delete bestMovies['Shutter Island'];                    // Deleting data
 
 console.log(bestMovies);
 
