1817ccb610dbf90506f3fefeb40de20e090d7973
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-05-02 00:40:28 +0530

    day 25

---
+++ Day 27/objectChecker.js
@@ -0,0 +1,42 @@
+
+/* Property Checker for JavaScript Objects */
+
+// Library of books
+const BOOKS = {
+    'Paulo Coelho' : 'The Alchemist',
+    'Harper Lee' : 'To Kill a Mockingbird',
+    'Marcus Aurelius' : ['Meditations','170 CE'],
+    'J D Salinger' : 'The Catcher in the Rye',
+    'Ichiro Kishimi' : 'The Courage to be Disliked',
+    'Rachel Ward' : 'Numbers',
+    'Rainbow Rowell' : 'Eleanor & Park'
+}
+
+// Function to check property
+function checkProp(obj,prop){
+    if(obj.hasOwnProperty(prop)){
+        return 'Property Found : ' + obj[prop];
+    }
+    return 'Property Does Not Exist !';
+}
+
+// Display
+let a = checkProp(BOOKS,'Marcus Aurelius');
+console.log('\n\n\t      --JavaScript Property Checker--');
+console.log('\n\t\tsearching...');
+console.log('\n\t\t' + a + '\n\n');
+
+// TIP: The if statement by default executes the commands inside it if the condition is True. 
+
+
+
+
+
+
+
+
+
+
+
+
+
