387058c4f727a4c69ed708b59aeb35cc5121b140
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-05-01 20:11:47 +0530

    update object.js

--- Day 24/objects.js
+++ Day 24/objects.js
@@ -1,7 +1,11 @@
 
 /* JavaScript Objects */
 
-// Below,the properties are stored as strings name, legs and tails.
+// In JavaScript, objects are similar to arrays, except that data in arrays can be accessed and modified using indexes but for objects, we access their data using what is called Properties.
+
+// Objects are useful for storing data in a structured way. We can store real life data like cats, dogs, spoons in objects.
+
+// In this example, the properties are stored as strings name, legs, tails and enemies. 
 const cat = {
     "name": "Whiskers",
     "legs": 4,
@@ -9,7 +13,7 @@ const cat = {
     "enemies": ["Water", "Dogs"]
 };
 
-// You can even have numbers as properties.
+// You can even have numbers as properties, and even omit the quotes for single-word strings because JavaScript will automatically typecast non-string properties as strings. 
 let myDog = {
     name: 'Bruno',
     'legs': 4,
@@ -17,17 +21,12 @@ let myDog = {
     'friends': ['Human', 'balls']
 };
 
+
 console.log("\n\nHi! I'm " + myDog.name + "\nI have " + myDog.legs + ' legs.\nAnd my best friend is ' + myDog.friends[0] + '!');
+
+console.log("\n\nHi! I'm " + cat.name + "\nI have " + cat.legs + ' legs.\nAnd I hate ' + cat.enemies[0] + '!');
 
 // TIP: JavaScript automatically typecasts non-string properties as strings.
 
 
 
-
-
-
-// In JavaScript, objects are similar to arrays, except that data in arrays can be accessed and modified using indexes but for objects, we access their data using what is called Properties.
-
-// Objects are useful for storing data in a structured way. We can store real life data like cats, dogs, spoons in objects.
-
-// You can even have numbers as properties, and even omit the quotes for single-word strings because JavaScript will automatically typecast non-string properties as strings. 
