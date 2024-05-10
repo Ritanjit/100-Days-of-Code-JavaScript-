dc3bd275108d47dc34887361caceb4cedbd02479
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-05-10 18:35:46 +0530

    update

--- Day 33/rescursion.js
+++ Day 33/rescursion.js
@@ -8,11 +8,6 @@
 // RECURSION 
 
 let array = [1,2,3,4,5,6,7,8,9,10];
-let add = 0;
-
-for(let i=0;i<array.length;i++){
-    add += array[i];
-}
 
 // [SUM] Recursive Function 
 function sum(arr,n){
