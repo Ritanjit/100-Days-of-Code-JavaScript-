9733f2d203acf5053b49b3f0ac07f9a49b0c4270
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-05-22 09:17:52 +0530

    46

---
+++ Day 47/spread.js
@@ -0,0 +1,29 @@
+
+/* Spread Operator in JavaScript */
+
+// ES5 had apply operator...
+let arr = [22,33,99,44,11,66];
+const MAX = Math.max.apply(null,arr);
+
+// ES6 uses spread operator...
+let arr2 = [45,35,125,65,95,15];
+const MAX2 = Math.max(...arr2);
+
+// Spread operator works only with functions and [expressions]...
+let temp = [];
+temp = [...arr];        // This will work
+temp = ...arr           // This will not work
+
+// TIP: spread operator (...arg) allows you to expand arrays and expressions where multiple arguments or elements are expected.
+
+// Console output
+console.log('\n\n-- Array Maximum in JavaScript --');
+console.log('\nUsing apply() operator : ' + MAX);
+console.log('\nUsing spread operator  : ' + MAX2);
+console.log('\n');
+
+
+
+
+
+
