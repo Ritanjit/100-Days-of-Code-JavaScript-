fbeb9aa45e32351c3bc52ba6140c9a1250aadc77
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2024-06-24 21:03:07 +0530

    update

--- Day 75/removeWhiteSpcaes.js
+++ Day 75/removeWhiteSpcaes.js
@@ -2,4 +2,4 @@
 
 let hello = "   Hello, World!  ";
 let wsRegex = /^(\s+)|(\s+)$/g; // Change this line
-let result = hello.replace(wsRegex,''); // Change this line-
+let result = hello.replace(wsRegex,''); // Change this line--
